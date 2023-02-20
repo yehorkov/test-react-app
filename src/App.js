a75216911fc75a39c2b0730be6349a9d5c09885d
Егор Коваль <yehor_kov@MacBook-Air-Egor.local>
2023-02-20 23:17:10 +0200

    End lesson about state in React

--- src/App.js
+++ src/App.js
@@ -1,17 +1,74 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
+
+// const Header = () => {
+//   return <h2>Hello world!</h2>
+// }
+
+// const Field = () => {
+//     const holder = 'Enter here';
+//     const styledField = {
+//         width: '300px'
+//     }
+//     return <input placeholder={holder} type='text' style={styledField}/>
+// }
+
+// class Field extends Component {
+//     render () {
+//         const holder = 'Enter here';
+//         const styledField = {
+//             width: '300px'
+//         }
+
+//         return <input placeholder={holder} type='text' style={styledField}/>
+//     }
+// }
+
+// const Btn = () => {
+//     const text = 'Log in';
+//     const logged = false;
+
+//     return <button>{logged ? 'Enter' : text}</button>
+// }
+
+class WhoAmI extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            years: 30,
+            text: '+++'
+        }
+    }
+
+    nextYear = () => {
+        this.setState(state => ({
+            years: state.years + 1
+        }))
+    }
+
+    render () {
+        const {name, surname, link} = this.props;
+        return (
+            <div>
+                <button onClick={this.nextYear}>{this.state.text}</button>
+                <h1>My name is {name}, surname - {surname}, age - {this.state.years}</h1>
+                <a href={link}>My profile</a>
+            </div>
+        )
+    }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+        {/* <Header/>
+        <Field/>
+        <Btn/> */}
+        <WhoAmI name='Yehor' surname='Koval' link='facebook.com'/>
+        <WhoAmI name='John' surname='Smith' link='instagram.com'/>
     </div>
   );
 }
 
+// export {Header};
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,20 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+// import { Header } from './App';
 
-const text = 'Hello World!';
+// const text = 'Hello World!';
 
-const elem = (
-    <div>
-        <h2 className='text'>Текст: {text}</h2>
-        <input type="text" />
-        <button tabIndex={0}>Click</button>
-    </div>
-)
+// const elem = (
+//     <div>
+//         <h2 className='text'>Текст: {text}</h2>
+//         <input type="text" />
+//         <label htmlFor=""></label>
+//         <button tabIndex={0}>Click</button>
+//     </div>
+// )
 
 // const elem = React.createElement('h2', {className: 'greetings'}, 'Hello World!');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  elem
+  <App/>
 );
